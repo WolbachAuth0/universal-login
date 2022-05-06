18d3caca751af353d006b6afd5b0bed30a8999e7
Aaron Wolbach <aaron.wolbach@auth0.com>
2022-05-06 11:30:16 -0400

    update prompts with branding

--- tasks/universal-login/set.js
+++ tasks/universal-login/set.js
@@ -24,6 +24,7 @@ async function setUniversalLogin() {
     const directory = themes.find(scheme => scheme.name == answers.theme).path
     const filename = path.join(directory, `template.html`)
     const branding = path.join(directory, `branding.json`)
+    const prompttext = path.join(directory, `prompts.json`)
     console.log(`\nsetting new universal login html template from ${filename}\n`)
 
     // read the HTML template into a string, and create a JSON body from the HTML template
@@ -39,6 +40,18 @@ async function setUniversalLogin() {
     const updateBranding = await api.updateBrandingSettings(params, require(branding))
     console.log('success updated branding')
     console.log(updateBranding)
+
+    // set the prompts
+    // See ... https://auth0.github.io/node-auth0/ManagementClient.html#updateCustomTextByLanguage
+    // and ... https://auth0.com/docs/customize/universal-login-pages/customize-login-text-prompts
+    const promptParams = {
+      prompt: 'login',
+      language: 'en',
+      body: require(prompttext)
+    }
+    const setPrompts = await api.prompts.updateCustomTextByLanguage(promptParams)
+    console.log('success set login prompts')
+    console.log(setPrompts)
   } catch (error) {
     console.log('error while updating new universal login.')
     console.error(error)
