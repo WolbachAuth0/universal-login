c10edd88950b89e800037a614ed3416cf6f30380
WolbachAuth0 <aaron.wolbach@okta.com>
2022-07-13 12:53:05 -0400

    refactor

--- tasks/universal-login/get.js
+++ tasks/universal-login/get.js
@@ -6,24 +6,26 @@ const ULtemplate = require('./../../lib/ul-template')
 get()
 
 async function get() {
-  try {
-    const api = await management([ 'read:branding' ])
 
-    console.log(`\nfetching new universal login branding from ${api.tenantName}\n`)
-    const branding = await ULbranding.read(api)
+  const api = await management([ 'read:branding' ])
+
+  // fetch the stuff from the tenant ...
+  const branding = await ULbranding.read(api)
+  const widget = await ULwidget.read(api)
+  const html = await ULtemplate.read(api)
+
+  // log it to console ...
+  if (branding) {
     console.log('\nbranding ...')
     console.log(branding)
-
-    console.log(`\nfetching new universal login widget from ${api.tenantName}\n`)
-    const widget = await ULwidget.read(api)
-    console.log('\nbranding ...')
-    console.log(widget)
-
-    const html = await ULtemplate.read(api)
-    console.log('\nhtml template ...')
-    console.log(html, '\n')
-  } catch (error) {
-    console.log(`error while fetching new universal login settings from ${api.tenantName}.`)
-    console.error(error)
   }
+  if (widget) {
+    console.log('\nwidget ...')
+    console.log(widget)
+  }
+  if (html) {
+    console.log('\nhtml template ...')
+    console.log(html)
+  }
+ 
 }
